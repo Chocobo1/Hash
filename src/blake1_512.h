858f069ba50f985d479bc5c84aceb73ed5c387f8
Chocobo1 <Chocobo1@users.noreply.github.com>
2018-12-12 15:03:30 +0800

    Update copyright date

--- src/blake1_512.h
+++ src/blake1_512.h
@@ -1,7 +1,7 @@
 /*
  *  Chocobo1/Hash
  *
- *   Copyright 2017 by Mike Tzou (Chocobo1)
+ *   Copyright 2017-2018 by Mike Tzou (Chocobo1)
  *     https://github.com/Chocobo1/Hash
  *
  *   Licensed under GNU General Public License 3 or later.
