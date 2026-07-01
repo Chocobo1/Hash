126d345bc2af4657d0aaca96641a30f0c7b61653
Chocobo1 <Chocobo1@users.noreply.github.com>
2026-07-01 14:29:18 +0800

    Suppress gcc `maybe-uninitialized` warning for 3rd party code

--- tests/main.cpp
+++ tests/main.cpp
@@ -9,9 +9,17 @@
  *  @license GPL3 <https://www.gnu.org/licenses/gpl-3.0-standalone.html>
  */
 
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
 #define CATCH_CONFIG_RUNNER
 #include "catch2/single_include/catch2/catch.hpp"
 
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
 
 int main(int argc, char *argv[])
 {
